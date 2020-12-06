20eda8be8971da4644b0e69f5068acd7fd753132
Zack Lee <cuinjune@gmail.com>
2020-12-06 05:22:28 -0500

    test

--- server/video-server.js
+++ server/video-server.js
@@ -144,7 +144,7 @@ var VideoServer = function (options, startedCallback) {
                       }
                       const options = {
                         method: "POST",
-                        uri: "http://localhost:3000/api/v1/flask/data",
+                        uri: "https://text-to-video-flask.herokuapp.com/:80/api/v1/flask/data",
                         body: { sentences: sentences },
                         json: true
                       };
